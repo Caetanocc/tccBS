c18bb9845f51db4ac0e1fb903140093c5b148a00
Caetanocc <claudiomiro.silva2@etec.sp.gov.br>
2022-10-01 13:14:19 -0300

    atualizar com remoto
    atualizar

--- conecta.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', '127.0.0.1');
-define('USUARIO', 'root');
-define('SENHA','');
-define('DB','etec3');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die(mysqli_error($conexao). 'Banco de dados OFF');
